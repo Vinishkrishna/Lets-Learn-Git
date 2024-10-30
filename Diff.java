ba63073eed6075e0bc43d53ec1ed44e8e75c1174
R Vinish Krishna <vinishkrishna05@gmail.com>
2024-10-30 19:30:30 +0530

    new files added

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int diff = num1 - num2;
+        System.out.println("The difference of " + num1 + " and " + num2 + " is " + diff);
+    }
+}
