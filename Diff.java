0eb3da77a106148d05746b2ed5a1ae77dd77eadc
Vinishkrishna <vinishkrishna05@gmail.com>
2024-10-31 00:08:26 +0530

    changes updated

--- Diff.java
+++ Diff.java
@@ -2,6 +2,7 @@ public class Diff {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 10;
+        int num3 = 15;
         int diff = num1 - num2;
         System.out.println("The difference of " + num1 + " and " + num2 + " is " + diff);
     }
